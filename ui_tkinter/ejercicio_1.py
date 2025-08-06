5b49242791e5ad1b9bce71243310eff16fc0f825
Jesus390 <jesusespinola90@gmail.com>
2025-08-06 00:25:39 -0300

    tk.Text() implementacion

--- ui_tkinter/ejercicio_1.py
+++ ui_tkinter/ejercicio_1.py
@@ -1,10 +1,33 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import StringVar
 
-import getpass
+import random
+import time
+
+print(time.ctime(time.time()))
+
+def ejecutar_servidor(cantidad=10):
+    print("Iniciando servidor...")
+    entry_text.insert(END, "Ejecutando servidor.\n")
+
 
 window = Tk()
+window.title("Servidor")
+window.geometry("300x300+100+100")
 
-window.title("Test Tkinter")
+var_entry_text = StringVar()
+
+frm_main = Frame(window, width=200, height=200, bg="blue")
+frm_main.pack()
+
+frm_btn = Frame(window, width=100, height=100, bg="red")
+frm_btn.pack(padx=10, pady=10)
+
+entry_text = Text(frm_main, height=10, width=50)
+entry_text.pack()
+
+btn_ejecutar_servidor = Button(frm_btn, text="Ejecutar Servidor", command=lambda: ejecutar_servidor())
+btn_ejecutar_servidor.pack()
 
 window.mainloop()
